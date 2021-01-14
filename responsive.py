9b71cbf662883710e2b6f7ed03be4b0179655e55
dave <daveg7lee@gmail.com>
2021-01-14 12:08:56 +0900

    Refactor

--- responsive.py
+++ responsive.py
@@ -1,16 +1,50 @@
+from math import ceil
 import time
+import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
 
-browser.get("https://dics.ga")
-browser.maximize_window()
+class ResponsizeTester:
+    def __init__(self, urls):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.browser.maximize_window()
+        self.urls = urls
+        self.sizes = [480, 960, 1366, 1920]
+        self.BROWSER_HEIGHT = 1000
 
-sizes = [320, 360, 480, 960, 1024, 1366, 1920]
+    def makeDir(self, directory):
+        try:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+        except OSError:
+            print('Error: Creating directory. ' + directory)
 
-for size in sizes:
-    browser.set_window_size(size, 1027)
-    time.sleep(1)
+    def screenshots(self, url):
+        self.browser.get(url)
+        directory = f"screenshots/{url.replace('https://', '').split('.')[0]}"
+        self.makeDir(directory)
+        for size in self.sizes:
+            self.browser.set_window_size(size, self.BROWSER_HEIGHT)
+            time.sleep(2)
+            scroll_size = self.browser.execute_script(
+                "return document.body.scrollHeight")
+            total_sections = ceil(scroll_size / self.BROWSER_HEIGHT)
+            for section in range(total_sections + 1):
+                self.browser.execute_script(
+                    f"window.scrollTo(0, {section * self.BROWSER_HEIGHT})")
+                self.browser.save_screenshot(
+                    f"{directory}/{size}x{section}.png")
+                time.sleep(2)
 
-browser.quit()
+    def start(self):
+        for url in self.urls:
+            self.screenshots(url)
+
+    def finish(self):
+        self.browser.quit()
+
+
+dicsTester = ResponsizeTester(["https://nomadcoders.co"])
+dicsTester.start()
+dicsTester.finish()
